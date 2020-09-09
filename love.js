9f4677690cc08e473a5a3b41a190ef7635d265f0
chetas11 <cbirmole@gmail.com>
2020-09-09 21:30:48 +0530

    layout

---
+++ love.js
@@ -0,0 +1,7 @@
+function cal(){
+    document.getElementById("msg").innerHTML = "I am so lucky to have you by my side today, tomorrow, and forever. You make my heart soar, my mind race, and my lips form a perfect smile. There are a million ways to say how much I love you that I truly don't know where to star"
+}
+
+function reset(){
+    location.reload()
+}
