8dc181375683fc1b877f62a1d5404267098eaf64
chetas11 <cbirmole@gmail.com>
2020-09-11 22:38:19 +0530

    complete

--- love.js
+++ love.js
@@ -1,15 +1,63 @@
-var yourName = document.getElementById("yname").innerText;
+var yourName 
+var partnerName  
+var clear = ""
 
-var partnerName = document.getElementById("pname").innerHTML;
+
 
 function cal(){
-    document.getElementById("msg").innerHTML = "I am so lucky to have you by my side today, tomorrow, and forever. You make my heart soar, my mind race, and my lips form a perfect smile. There are a million ways to say how much I love you that I truly don't know where to star"
+    yourName = document.getElementById("yname").value;
+    partnerName = document.getElementById("pname").value;
+    var random = Math.floor(Math.random()*100)+1;
+
+    if(yourName !== "" && partnerName !== ""){
+        if(random > 80 ){
+            document.getElementById("msg").innerHTML = "Together you act as one. Together you smile. Together you cry. You are not alone anymore. Love is about two people who are destined to be one ♡"
+            document.getElementById("per").innerHTML = random+"%";
+        }else if(random > 60){
+            document.getElementById("msg").innerHTML = "The ideal couple is the one who keeps everything simple yet very beautiful. They keep the love stronger each time. Love is what matters to them. For them, it is the most important thing ♡"
+            document.getElementById("per").innerHTML = random+"%";
+        }else if(random  > 40){
+            document.getElementById("msg").innerHTML = "Behind every wonderful couple are two wonderful people who fought hard to win every battle that comes their way ♡ "
+            document.getElementById("per").innerHTML = random+"%";
+        }else{
+            document.getElementById("msg").innerHTML = "For a couple to stay forever they must consistently communicate to each other. They must know how to keep the fire burning. Never get afraid of being hurt. Love is a game. Play it better ♡"
+            document.getElementById("per").innerHTML = random+"%";
+        }
+
+    }else{
+        document.getElementById("msg").innerHTML = "Please fill all the details..."
+    }
+  
+
+
+
+    
+
+    // if(yourName[0]==="C" && partnerName[0] === "A"){
+    //     document.getElementById("msg").innerHTML = "Wohooo wataaaa match !!! Zakassss ♡"
+    //     document.getElementById("per").innerHTML = random+"%";
+    // }else{
+    //     document.getElementById("per").innerHTML = random+"%";
+    //     document.getElementById("msg").innerHTML = "Great!"
+    // }
+
 }
 
+var calBtn = document.getElementById('btn');
+calBtn.addEventListener('click', cal);
+
 function reset(){
-    location.reload()
-    console.log(yourName, partnerName)
+    document.getElementById("yname").value = ""
+    document.getElementById("pname").value = ""
+    document.getElementById("msg").innerHTML = clear
+    document.getElementById("per").innerHTML = clear
 }
+
+
+
+
+
+
 
 
 
